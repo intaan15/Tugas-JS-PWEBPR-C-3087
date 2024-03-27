594c21d972d7d59066dc17f7cfbe110c99746029
intaan lailatul <130087880+intaan15@users.noreply.github.com>
2024-03-27 11:26:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function oke(){
     document.getElementById("yes").remove();
     document.getElementById("noo").remove();
     document.getElementsByClassName("header")[0].innerHTML = "Anda Berbohong!"
-    document.getElementsByClassName("detective")[0].src = "assets/Teknisi.png"
+    document.getElementsByClassName("detective")[0].src = "Assets/Teknisi.png"
     document.getElementsByClassName("teks")[0].innerHTML = "Anda adalah seorang Teknisi"
     document.getElementsByClassName("container")[0].style.backgroundColor="#E12E2E"
 }
@@ -10,7 +10,7 @@ function not(){
     document.getElementById("yes").remove();
     document.getElementById("noo").remove();
     document.getElementsByClassName("header")[0].innerHTML = "Anda Benar!"
-    document.getElementsByClassName("detective")[0].src = "assets/Mahasiswa.png"
+    document.getElementsByClassName("detective")[0].src = "Assets/Mahasiswa.png"
     document.getElementsByClassName("teks")[0].innerHTML = "Anda adalah seorang Mahasiswa"
     document.getElementsByClassName("container")[0].style.backgroundColor ="#6FD240"
 }
